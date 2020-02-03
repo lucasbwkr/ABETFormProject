0fc3243058ca15863d36463e2741edca388068ec
lucasb <lucasbwkr@gmail.com>
2020-02-03 12:44:18 -0600

    Added a few comments

--- MainGUI/app.py
+++ MainGUI/app.py
@@ -1,23 +1,29 @@
+# Source: https://www.youtube.com/watch?v=Z1RJmh_OqeA
+
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///results.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Settings up database which will contain the results here.
+# Name of the DB is results.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///results.db'
+# Added to remove a deprecated feature (should not cause any issues)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Creating our database. 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     classname = db.Column(db.String(50), nullable = False)
     results = db.Column(db.String(500), nullable = False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
     def __repr__(self):
         return '<Results %r>' % self.id
 
+# Main page of our web app.
 @app.route('/')
 def index():
     return render_template('index.html')
